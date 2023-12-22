3f52207002c5cf8364cb9580ee154cd96915d0e0
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-22 08:38:24 -0800

    check pip3 install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install flask with puppet
 exec { 'install flask':
-  command => 'pip install flask==2.1.0',
-  path => 'usr/bin',
+  command => 'pip3 install flask==2.1.0',
+  path => ['usr/bin'],
 }
