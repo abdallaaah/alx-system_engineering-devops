37952ef2b600666862a9b0123c07b7681659c170
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-22 08:43:19 -0800

    check this fincal install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install flask with puppet
-exec { 'install flask':
-  command => 'pip3 install flask==2.1.0',
-  path => ['usr/bin'],
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
